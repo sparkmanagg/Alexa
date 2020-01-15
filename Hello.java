bde88271d89c29f84ee526ada68b131e41d626cf
sparkmanagg <59941196+sparkmanagg@users.noreply.github.com>
2020-01-15 18:36:39 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*
+ * Hello World
+ **********************************************************
+ * Alexa Sparkman 
+ * Jan 15 2020
+ * 255 and 901
+ */
+
+public class Hello {
+
+ 	public static void main(String[] args) {
+
+ 	 System.out.println("<Alexa Sparkman>");
+ 	 System.out.println("<Introduction to the Course>");
+ 	 System.out.println("<Project Description>");
+ 	 System.out.println("<Jan 15 2020>");
+ 	 System.out.println("<255 and 901>");
+ 	 System.out.println();
+ 	 }
+ }
